a200fae514cc7dd32d0739a862364f6921533fb4
Dominik <rakdominikrak@gmail.com>
2021-05-05 19:50:53 +0200

    Initial models in library

--- library/models.py
+++ library/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from datetime import datetime    
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, )
+    last_name = models.CharField(max_length=100)
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=20)
+    authors = models.ManyToManyField(Author)
+    genres = models.ManyToManyField(Genre)
+    publisher = models.ForeignKey(Publisher, on_delete=models.SET_NULL, null=True)
+
+class BookCopy(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    copy_number = models.IntegerField()
+
+class UserCard(models.Model):
+    #TODO user who is owner of card
+    join_date = models.DateTimeField(default=datetime.now, blank=True)
+
+class Borrowing(models.Model):
+    user_card = models.ForeignKey(UserCard, on_delete=models.CASCADE)
+    book_copy = models.ForeignKey(BookCopy, on_delete=models.CASCADE)
+    borrow_date = models.DateTimeField(default=datetime.now, blank=True)
+    return_date = models.DateTimeField(blank=True)
